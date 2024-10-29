a75f7b2d3680201e7f569900e1951f536fe371ec
LOWZIMIN <lowzm.23@ichat.sp.edu.sg>
2024-10-29 10:32:07 +0800

    bmi.py

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,25 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    return 
+
+def calc_average():
+    print("calc_average")
+
+def find_min_max():
+    return
+
+def sort_temperature():
+    return
+
+def calc_median_temperature():
+    return
+
+def main(): 
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python") 
+    display_main_menu() 
+    num_list = get_user_input()
+
+if __name__ == "__main__": 
+    main() 
